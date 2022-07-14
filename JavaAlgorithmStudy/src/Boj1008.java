b50804d592439c5cb97e34bbb73b9fd4d2e29d5e
Juna Kang <apples707@naver.com>
2022-07-14 11:30:27 +0900

    Java Boj1008

---
+++ JavaAlgorithmStudy/src/Boj1008.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Boj1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1 = sc.nextDouble();
+        double num2 = sc.nextDouble();
+        sc.close();
+        System.out.println(num1/num2);
+    }
+}
