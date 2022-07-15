8673d12a8e1c79477049257d29409ff2ade6b37d
Juna Kang <apples707@naver.com>
2022-07-15 10:44:19 +0900

    Java Boj18108

---
+++ JavaAlgorithmStudy/src/Boj18108.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Boj18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.println(num1-543);
+    }
+}
