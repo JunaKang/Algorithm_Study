0a59d70304e41b2ade8307ed340a0440a7454474
Juna Kang <apples707@naver.com>
2022-07-18 14:16:11 +0900

    Java Boj14681

---
+++ JavaAlgorithmStudy/src/Boj14681.java
@@ -0,0 +1,35 @@
+//import java.util.Scanner;
+//
+//public class Boj14681 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num1 = sc.nextInt();
+//        int num2 = sc.nextInt();
+//        if (num1 > 0 && num2 > 0) {
+//            System.out.println(1);
+//        } else if (num1 < 0 && num2 > 0) {
+//            System.out.println(2);
+//        } else if (num1 < 0 && num2 < 0) {
+//            System.out.println(3);
+//        } else System.out.println(4);
+//    }
+//}
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj14681 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+        if (num1 > 0 && num2 > 0) {
+            System.out.println(1);
+        } else if (num1 < 0 && num2 > 0) {
+            System.out.println(2);
+        } else if (num1 < 0 && num2 < 0) {
+            System.out.println(3);
+        } else System.out.println(4);
+    }
+}
