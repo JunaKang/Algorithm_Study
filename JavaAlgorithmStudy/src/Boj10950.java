9507dfbfec43d192e3cba48258777dba031be306
JunaKang <apples707@naver.com>
2022-07-25 16:14:11 +0900

    Java Boj10950

---
+++ JavaAlgorithmStudy/src/Boj10950.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Boj10950 {
+    public static void main(String[] args)  {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+    }
+}
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        int num1 = Integer.parseInt(br.readLine());
+//
+//        StringTokenizer st;
+//        StringBuilder sb = new StringBuilder();
+//        br.close();
+//
+//        for (int i = 0; i<num1; i++) {
+//            st = new StringTokenizer(br.readLine()," ");
+//            sb.append(Integer.parse)
+//        }
+//        System.out.println();
+
+
+
